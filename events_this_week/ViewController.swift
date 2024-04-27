fe6084f5b8593eefa3c5a24aa66da2d4cf5c1f0b
NikhilBhatia6 <nikhilbhatia2323@gmail.com>
2024-04-27 15:07:26 +0530

    Initial Commit

---
+++ events_this_week/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  events_this_week
+//
+//  Created by Student on 27/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
